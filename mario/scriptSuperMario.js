5d9bb49f9890b532315082d98f2f4e9d5e63dfd1
JadsonPS <jadsonps12345@gmail.com>
2023-07-30 16:52:19 -0300

    correção de bug do mario andar mais do que o esperado

--- mario/scriptSuperMario.js
+++ mario/scriptSuperMario.js
@@ -19,38 +19,43 @@ function mover(direcao) {
     if (direcao == 'esquer') {
         
 
-        if(passosEsquerda%2 === 0){
-            document.getElementById('mario').style.backgroundImage = 'url(./mario/imagens/frames/corte/esquerda-0.png)';
-            document.getElementById('mario').style.left = (passos*10) + 'px';
-        }else{
-            document.getElementById('mario').style.backgroundImage = 'url(./mario/imagens/frames/corte/esquerda-1.png)';
-            document.getElementById('mario').style.left = (passos*10) + 'px';
-        }
-        passosEsquerda += 1;
-        passos -= 1;
-        if(passos == -10) {
-            console.log("10 passos para a esquerda");
-            document.getElementById('mario').style.backgroundImage = 'url(./mario/imagens/frames/corte/comemoracao-mario-fire.png)';
+        if(passos >= -9){
+            if(passosEsquerda%2 === 0){
+                document.getElementById('mario').style.backgroundImage = 'url(./mario/imagens/frames/corte/esquerda-0.png)';
+                document.getElementById('mario').style.left = (passos*10) + 'px';
+            }else{
+                document.getElementById('mario').style.backgroundImage = 'url(./mario/imagens/frames/corte/esquerda-1.png)';
+                document.getElementById('mario').style.left = (passos*10) + 'px';
+            }
+            passosEsquerda += 1;
+            passos -= 1;
+            if(passos == -10) {
+                console.log("10 passos para a esquerda");
+                document.getElementById('mario').style.backgroundImage = 'url(./mario/imagens/frames/corte/comemoracao-mario-fire.png)';
+            }
         }
 
     /* Movimento para direita */    
     } else if (direcao == 'direit') {
         
-            
-         if(passosDireita%2 === 0){
-            document.getElementById('mario').style.backgroundImage = 'url(./mario/imagens/frames/corte/direita-0.png)';
-            document.getElementById('mario').style.left = (passos*10) + 'px';
-        }else{
-            document.getElementById('mario').style.backgroundImage = 'url(./mario/imagens/frames/corte/direita-1.png)';
-            document.getElementById('mario').style.left = (passos*10) + 'px';
-        }
-        passosDireita += 1;
-        passos += 1;
-        if(passos == 10) {
-            console.log("10 passos para a direita");
-            document.getElementById('mario').style.backgroundImage = 'url(./mario/imagens/frames/corte/mario-explosion.png)';
-            
-        }
+        if(passos <= 9){
+            if(passosDireita%2 === 0){
+                document.getElementById('mario').style.backgroundImage = 'url(./mario/imagens/frames/corte/direita-0.png)';
+                document.getElementById('mario').style.left = (passos*10) + 'px';
+            }else{
+                document.getElementById('mario').style.backgroundImage = 'url(./mario/imagens/frames/corte/direita-1.png)';
+                document.getElementById('mario').style.left = (passos*10) + 'px';
+            }
+            passosDireita += 1;
+            passos += 1;
+            if(passos == 10) {
+                console.log("10 passos para a direita");
+                document.getElementById('mario').style.backgroundImage = 'url(./mario/imagens/frames/corte/mario-explosion.png)';
+            }
+        }  
+        
+
+        
  
     }
 }
